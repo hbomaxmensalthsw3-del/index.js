c2cd4dd605a8b8db4106db4e811706108a64b980
hbomaxmensalthsw3-del <hbomaxmensalthsw3@gmail.com>
2026-05-21 20:50:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+console.log("STARTING APP...");
+
 const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Servidor online 🔥");
-});
-
-app.get("/auth/discord/callback", (req, res) => {
-    res.send("OAuth funcionando 🔥");
+    res.send("OK FUNCIONANDO 🔥");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("Servidor online");
+    console.log("SERVER RUNNING ON PORT " + PORT);
 });
